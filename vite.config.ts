eb2500adbb69efa378d2c7be4f8c495065e72e5d
Ahmet Kaan Toprakçıoğlu <56438853+ahmettoprakcioglu@users.noreply.github.com>
2025-05-07 12:15:09 +0300

    chore: update Vite configuration to increase chunk size warning limit and remove unused server proxy settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -11,15 +10,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-      },
-    },
-  },
   build: {
+    chunkSizeWarningLimit: 1500,
     rollupOptions: {
       external: ['@rollup/rollup-linux-x64-gnu'],
     },
